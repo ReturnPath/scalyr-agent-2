68ed1b35351844fb1b62afa789fbf1e0c724687b
Bob Potter <bobby.potter@gmail.com>
2016-03-02 22:21:02 -0800

    global_log

--- scalyr_agent/builtin_monitors/docker_monitor.py
+++ scalyr_agent/builtin_monitors/docker_monitor.py
@@ -116,17 +116,17 @@ class DockerRequest( object ):
         #first line is response code
         line = self.__request_stream.read_request(0.1)
         if not line:
-            print 'Bailing at first line'
+            global_log.info( 'Bailing at first line' )
             return
 
-        print 'line: %s' % line
+        global_log.info( 'line: %s' % line )
         match = re.match( '^(\S+) (\d+) (.*)$', line.strip() )
         if not match:
-            print 'Bailing at re.match'
+            global_log.info( 'Bailing at re.match' )
             return
 
         if match.group(1).startswith( 'HTTP' ):
-            print 'Bailing at HTTP check'
+            global_log.info( 'Bailing at HTTP check' )
             self.response_code = int( match.group(2) )
             self.response_message = match.group(3)
             self.__headers = []
